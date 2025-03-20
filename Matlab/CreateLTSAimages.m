87d928db886d1da9fa641d049bc9477243bb9d7a
asimonis <anne.simonis@gmail.com>
2025-03-20 13:18:37 -0700

    Update CreateLTSAimages.m

--- Matlab/CreateLTSAimages.m
+++ Matlab/CreateLTSAimages.m
@@ -5,11 +5,13 @@
     t2 = timenum('07/16/2023 15:00:00',6); %Last hour of deployment
     StartTimes = t1:hours(1):t2;
     
-    for t = 1:length(StartTimes) 
+    %for t = 1:length(StartTimes) 
+
+        for t = 1:5
         PARAMS.ltsa.plot.dnum = datenum(StartTimes(t));
         read_ltsadata
         plot_triton
 
-        PNGName = sprintf('%s%s%s','CalCurCeas_test_',datestr(StartTimes(t),'yyyymmdd'),'.png')
+        PNGName = sprintf('%s%s%s','CalCurCeas_test_',datestr(StartTimes(t),'yyyymmdd_HHMM'),'.png')
         saveas(gca,PNGName)
     end
