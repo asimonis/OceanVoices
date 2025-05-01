c1bff0e9ef45502062818f1e511a0f999c97aa24
Anne Simonis <anne.simonis@gmail.com>
2025-04-30 21:15:41 -0700

    ID files w/spectral banding
    Use panoptes aggregator to extract answers from questions.
    Then identify files with spectral banding for secondary review (Zooniverse Processing Gg.qmd).
    Lastly, translate data points to actual frequency values (DataPointTranslation.qmd)

--- Matlab/CreateLTSAimages.m
+++ Matlab/CreateLTSAimages.m
@@ -1,17 +1,22 @@
 %save images of Triton ltsas each hour
 
+
+global PARAMS
+
+
     %set new datetime
-    t1 = timenum('07/11/2023 20:00:00',6); %First hour of deployment
-    t2 = timenum('07/16/2023 15:00:00',6); %Last hour of deployment
+    t1 = timenum('11/11/2024 02:00:00',6); %First hour of deployment
+    t2 = timenum('11/24/2024 14:00:00',6); %Last hour of deployment
     StartTimes = t1:hours(1):t2;
     
-    %for t = 1:length(StartTimes) 
+set(gcf, 'Position', [-1.4490   -0.2325    0.8781    1.1458])
 
-        for t = 1:5
+cd('C:\Users\annes\Documents\CalCurCEAS\CalCurCeas_024')
+   for t = 1:length(StartTimes) 
         PARAMS.ltsa.plot.dnum = datenum(StartTimes(t));
         read_ltsadata
         plot_triton
 
-        PNGName = sprintf('%s%s%s','CalCurCeas_test_',datestr(StartTimes(t),'yyyymmdd_HHMM'),'.png')
+        PNGName = sprintf('%s%s%s','CalCurCeas_024_',datestr(StartTimes(t),'yyyymmdd_HHMM'),'.png')
         saveas(gca,PNGName)
     end
